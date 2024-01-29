6f24a9911cbbc7ed6fd5f70a9e9b8eafca2d541d
Blake Lim <blakelim007@gmail.com>
2024-01-29 22:08:46 +0000

    Completed typescript-primitives-and-variables exercise

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,19 @@
+const fullName: string = 'Blake Lim';
+const isCool: boolean = true;
+const totalPets: number = 0;
+
+console.log('value of Full Name:', fullName);
+console.log('value of Is Cool:', isCool);
+console.log('value of Total Pets:', totalPets);
+
+console.log('type of:', typeof fullName);
+console.log('type of:', typeof isCool);
+console.log('type of:', typeof totalPets);
+
+const unknown: null = null;
+console.log('value of unknown:', unknown);
+console.log('type of:', typeof unknown);
+
+let never: undefined;
+console.log('value of never:', never);
+console.log('type of:', typeof never);
