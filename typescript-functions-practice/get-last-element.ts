a042e188718325eb9badf0d953127df417d023bb
Blake Lim <blakelim007@gmail.com>
2024-02-01 22:22:28 +0000

    work in prog

--- typescript-functions-practice/get-last-element.ts
+++ typescript-functions-practice/get-last-element.ts
@@ -1,4 +1,5 @@
 /* exported getLastElement */
-// function getLastElement(array: string[]): string {
-//   return array.pop()
-// };
+function getLastElement(array: string[]): string {
+  return array[array.length - 1];
+  // return array.pop()
+}
