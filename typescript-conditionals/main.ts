d47f2f83f84a6d99c93ec8164f0a733142f6d6ea
Blake Lim <blakelim007@gmail.com>
2024-02-01 20:53:21 +0000

    Completed typescript-conditionals exercise

---
+++ typescript-conditionals/main.ts
@@ -0,0 +1,125 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro,
+            recommendMovie
+ */
+function isUnderFive(number: number): boolean {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isUnderFive(6));
+
+function isEven(number: number): boolean {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isEven(8));
+
+function startsWithJ(string: string): boolean {
+  if (string.charAt(0) === 'J') {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(startsWithJ('JavaScript'));
+
+interface Person {
+  name: string;
+  age: number;
+}
+function isOldEnoughToDrink(person: Person): boolean {
+  if (person.age > 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+const blake = {
+  name: 'Blake Lim',
+  age: 25,
+};
+console.log(isOldEnoughToDrink(blake));
+
+function isOldEnoughToDrive(person: Person): boolean {
+  if (person.age > 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isOldEnoughToDrive(blake));
+
+function isOldEnoughToDrinkAndDrive(person: Person): unknown {
+  if (person) {
+    return false;
+  }
+}
+console.log(isOldEnoughToDrinkAndDrive(blake));
+
+function categorizeAcidity(pH: number): string {
+  if (pH === 7) {
+    return 'neutral';
+  } else if (pH > 7 && pH <= 14) {
+    return 'base';
+  } else if (pH < 7 && pH >= 0) {
+    return 'acid';
+  } else {
+    return 'invalid pH level';
+  }
+}
+console.log(categorizeAcidity(0));
+
+function introduceWarnerBro(name: string): string {
+  switch (name) {
+    case 'yakko':
+      return `We're the warner brothers!`;
+      break;
+    case 'wakko':
+      return `We're the warner brothers!`;
+      break;
+    case 'dot':
+      return `I'm cute~`;
+      break;
+    default:
+      return 'Goodnight everybody!';
+  }
+}
+console.log(introduceWarnerBro('dot'));
+
+function recommendMovie(genre: string): string {
+  switch (genre) {
+    case 'action':
+      return 'Pacific Rim';
+      break;
+    case 'comedy':
+      return 'Migration';
+      break;
+    case 'horror':
+      return 'Prometheus';
+      break;
+    case 'drama':
+      return 'Whiplash';
+      break;
+    case 'musical':
+      return 'La La Land';
+      break;
+    case 'sci-fi':
+      return 'Star Wars';
+      break;
+    default:
+      return 'Genre not recognized. Choose between action, comedy, horror, drama, musical, sci-fi';
+  }
+}
+console.log(recommendMovie('action'));
---
+++ typescript-conditionals/typescript-conditionals-quiz-notes.md
@@ -0,0 +1,52 @@
+# typescript-conditionals-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- Give 6 examples of comparison operators.
+  <, >, <=, =>, !=, ==
+- What data type do comparison expressions evaluate to?
+  Booleans
+- What is the purpose of an `if` statement?
+  To evaluate a condition in parenthesis, if true then the code block is executed
+- Is `else` required in order to use an `if` statement?
+  No, else is an optional block and is executed if condition of if statement is false
+- Describe the syntax (structure) of an `if` statement.
+  In an if syntax if(...), the parentheses is where the conditional statement would go
+- What are the three logical operators?
+  The || OR, $$ AND, and ! NOT operators
+- How do you compare two different expressions in the same condition?
+  By using the switch statement
+- What is the purpose of a `switch` statement?
+  To evaluate an expression passed into the parenthesis and compares the result to different cases in the code block
+- Is a `default` clause required in order to use a `switch` statement?
+  No, it is optional, but it is good to have in case none of the cases match the result of the expression passed
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
