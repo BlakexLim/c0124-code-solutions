319507745ed46d91954f30cbd1fdbe40aee330aa
Blake Lim <blakelim007@gmail.com>
2024-02-04 20:08:36 +0000

    Completed typescript-loops-practice exercise

--- typescript-loops-practice/odd-or-even.ts
+++ typescript-loops-practice/odd-or-even.ts
@@ -1,4 +1,12 @@
 /* exported oddOrEven */
-// function oddOrEven(numbers: number[]): string[] {
-
-// }
+function oddOrEven(numbers: number[]): string[] {
+  const result = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      result.push('even');
+    } else {
+      result.push('odd');
+    }
+  }
+  return result;
+}
