7c41c96fafe2ffaf871f5505c863e8ed08cffe43
Blake Lim <blakelim007@gmail.com>
2024-02-05 05:29:13 +0000

    Completed object-manipulation-1 exercise

--- object-manipulation-1/src/to-object.ts
+++ object-manipulation-1/src/to-object.ts
@@ -1,8 +1,8 @@
 /* exported toObject */
-function toObject(keyValuePair: Record<string, unknown>): any {
+function toObject(keyValuePair: [string, unknown]): Record<string, unknown> {
   const key = keyValuePair[0];
   const value = keyValuePair[1];
-  const object = {};
+  const object: Record<string, unknown> = {};
   object[key] = value;
   return object;
 }
