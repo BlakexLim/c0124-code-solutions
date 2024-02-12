f45623351c24fe55ce94631d1871cfa43218aab2
Blake Lim <blakelim007@gmail.com>
2024-02-12 18:25:41 +0000

    work in prog

--- array-manipulation-2/src/take.ts
+++ array-manipulation-2/src/take.ts
@@ -1,10 +1,10 @@
 /* exported take */
-function take(array: unknown[], count: number): unknown[] {
-  const result = [];
+function take(array: unknown[], count: number): unknown {
+  let result;
   for (let i = 0; i < array.length; i++) {
     if (array[i] === count) {
-      const newArr = array.length;
-      result.push(newArr);
+      const newArr = array[i];
+      result = newArr;
     }
   }
   return result;
