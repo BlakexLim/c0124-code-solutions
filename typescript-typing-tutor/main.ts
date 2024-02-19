05c83b50ce56704a19cd330382b8071d6abf6358
Blake Lim <blakelim007@gmail.com>
2024-02-19 01:15:33 +0000

    work in prog

--- typescript-typing-tutor/main.ts
+++ typescript-typing-tutor/main.ts
@@ -1,14 +1,14 @@
 // query the DOM for ALL span elements and assign to variable
-// const $characters = document.querySelectorAll('span');
-// if (!$characters) throw new Error('$characters query failed');
+const $characters = document.querySelectorAll('span');
+if (!$characters) throw new Error('$characters query failed');
 
-// let i = 0;
+let i = 0;
 
-// document.addEventListener('keydown', (event: KeyboardEvent) => {
-//   if (event.key === $characters[i]) {
-//     $characters[i].className = 'correct next';
-//     i++;
-//   } else {
-//     $characters[i].className = 'wrong';
-//   }
-// });
+document.addEventListener('keydown', (event: KeyboardEvent) => {
+  if (event.key) {
+    $characters[i].className = 'correct next';
+    i++;
+  } else {
+    $characters[i].className = 'wrong';
+  }
+});
