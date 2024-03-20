11615f22e15e8e30b49bfbf8016a0d678f629d1e
Blake Lim <blakelim007@gmail.com>
2024-03-20 00:04:00 +0000

    delete text from imageDescription.tsx

--- react-props/my-app/src/ImageDescription.tsx
+++ react-props/my-app/src/ImageDescription.tsx
@@ -7,14 +7,7 @@ export function ImageDescription({ des }: Props) {
     <div>
       <p>
         {des}
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam
-        incidunt sit dolorum accusantium sapiente reiciendis maxime dolores
-        ullam delectus, commodi placeat eveniet, quam voluptates facere et
-        magnam architecto quaerat velit voluptas rerum quos asperiores quis in!
-        Nemo aut aliquam quas dolore quaerat, error numquam odio modi eos vero.
-        Modi nisi eos autem? Iusto hic impedit aspernatur labore quos nesciunt?
-        Ad dolore pariatur aperiam nemo, maiores laboriosam quam quod nisi
-        ipsum, dicta sunt laborum similique repellat?
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
       </p>
     </div>
   );
