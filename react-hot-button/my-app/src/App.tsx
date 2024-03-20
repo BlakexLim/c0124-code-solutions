c5525386520dfa104afe0d0988a3fd329fd5eff6
Blake Lim <blakelim007@gmail.com>
2024-03-20 19:32:51 +0000

    Completed react-hot-button exercise

---
+++ react-hot-button/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { HotButton } from './button';
+
+const btnTxt = 'Hot Button';
+function App() {
+  return (
+    <>
+      <HotButton label={btnTxt} />
+    </>
+  );
+}
+
+export default App;
