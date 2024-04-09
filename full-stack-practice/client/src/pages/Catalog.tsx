7c91a12b81b2f9c3f711ffc8078e32da76f0ed6d
Blake Lim <blakelim007@gmail.com>
2024-04-09 00:23:14 +0000

    figuring out 500 error, can't retrieve one item

---
+++ full-stack-practice/client/src/pages/Catalog.tsx
@@ -0,0 +1,71 @@
+import './Pages.css';
+import { type Product } from '../lib';
+import { useEffect, useState } from 'react';
+// import { readCatalog } from '../lib';
+import { Link } from 'react-router-dom';
+import { toDollars } from '../lib';
+
+export function Catalog() {
+  const [items, setItems] = useState<Product[]>([]);
+  const [error, setError] = useState<unknown>();
+  const [loading, setLoading] = useState(true);
+
+  // use Effect to asynchronously fetch product data from the array of products in data.ts
+  useEffect(() => {
+    async function loadCatalog() {
+      try {
+        const res = await fetch('/api/products');
+        if (!res.ok) throw new Error(`Response error, status ${res.status}`);
+        const result = await res.json();
+        setItems(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadCatalog();
+  }, []);
+
+  // if data is taking a while to load, display a msg to the user
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // if error occurs while fetching, let user know
+  if (error) {
+    return (
+      <div>
+        Error! Unable to retrieve products, <Link to="/">try again</Link>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <h2 className="page-title">Catalog</h2>
+      <div className="row">
+        {items.map((item) => (
+          <div key={item.productId} className="card">
+            <ProductCard product={item} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+// create a function to render the product layout, then pass into Catalog JSX
+type CardProps = {
+  product: Product;
+};
+function ProductCard({ product }: CardProps) {
+  const { productId, name, imageUrl, price, shortDescription } = product;
+  return (
+    <Link to={`/details/${productId}`} className="product-card">
+      <h4 className="card-title">{name}</h4>
+      <img className="card-img" src={imageUrl}></img>
+      <h5 className="card-price">{toDollars(price)}</h5>
+      <p className="card-s-desc">{shortDescription}</p>
+    </Link>
+  );
+}
---
+++ full-stack-practice/client/src/pages/Details.tsx
@@ -0,0 +1,77 @@
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { AiFillCaretLeft } from 'react-icons/ai';
+import { useEffect, useState } from 'react';
+import { toDollars, type Product } from '../lib';
+// import { readProduct } from '../lib';
+// import { NotFound } from './NotFound';
+
+export function Details() {
+  const { productId } = useParams();
+  const navigate = useNavigate();
+  const [product, setProduct] = useState<Product>();
+  const [error, setError] = useState<unknown>();
+  const [loading, setLoading] = useState(true);
+
+  function handleAdd(): void {
+    alert(`${name} added to cart`);
+    navigate('/');
+  }
+
+  // use Effect to asynchronously fetch loadProduct data
+  useEffect(() => {
+    async function loadProduct(productId: number) {
+      try {
+        const res = await fetch(`/api/products/${productId}`);
+        console.log(res);
+        if (!res.ok) throw new Error(`Response Error, status ${res.status}`);
+        const result = await res.json();
+        setProduct(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    if (productId) {
+      setLoading(true);
+      loadProduct(+productId);
+    }
+  }, [productId]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error || !product) {
+    console.error('Fetch Error:', error);
+    return (
+      <div>
+        <p>Error! {error instanceof Error ? error.message : 'Unknown Error'}</p>
+      </div>
+    );
+  }
+
+  const { name, imageUrl, price, longDescription } = product;
+  return (
+    <div className="product-container">
+      <Link to="/" className="back-btn">
+        <AiFillCaretLeft />
+        Back to catalog
+      </Link>
+      <div className="p-row">
+        <div>
+          <h2 className="p-name">{name}</h2>
+        </div>
+        <div>
+          <img src={imageUrl} className="image" />
+        </div>
+        <div>
+          <h3 className="p-price">{toDollars(price)}</h3>
+        </div>
+        <div>
+          <p className="p-l-desc">{longDescription}</p>
+        </div>
+      </div>
+      <button className="add" onClick={handleAdd}>
+        Add to cart
+      </button>
+    </div>
+  );
+}
